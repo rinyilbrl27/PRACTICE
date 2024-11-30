aa2d1da6d6b81fbf51d0c3bdfee458bb52bf9013
BORELA, Renniel R. <borelarenniel@gmail.com>
2024-11-30 17:26:45 +0800

    Delete student-record-list directory

--- student-record-list/js/index.js
+++
@@ -1,6 +0,0 @@
-function toggleCheckboxes(selectAll) {
-    const checkboxes = document.querySelectorAll('.subject-checkbox');
-    checkboxes.forEach(checkbox => {
-        checkbox.checked = selectAll.checked;
-    });
-}
